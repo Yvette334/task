b1234fd8cdf561179f3a454511d6bc6e548c5faa
Yvette334 <y.muhoracye@alustudent.com>
2025-09-30 19:43:15 +0200

    Included the filter for the task

--- script.js
+++ script.js
@@ -112,13 +112,6 @@ function markComplete(taskid) {
     showtask()
   }
 }
-function alltask(taskid) {
-  return sampletasks.filter((status) => {
-    return taskid.status.include(taskid)
-  })
-}
-
-
 function edit(taskid) {
   const task = sampletasks.find(function (task) {
     return task.id === taskid
@@ -135,6 +128,52 @@ function edit(taskid) {
     showtask()
     save()
   }
+}
+function alltask() {
+  showtask()
+}
+function allcompleted() {
+  let completedtask = sampletasks.filter((task) => task.status === "completed")
+  displayfiltered(completedtask)
+}
+function allpending() {
+  let pendingtask = sampletasks.filter((task) => task.status === "pending")
+  displayfiltered(pendingtask)
+}
+function displayfiltered(tasks) {
+  taskdiv.innerHTML = ""
+  tasks.forEach(function (taskitem) {
+    let taskelement = document.createElement("div")
+    taskelement.className =
+      "flex items-center justify-between p-3 mb-2 border rounded-lg"
+    if (taskitem.status === "completed") {
+      taskelement.className =
+        "bg-green-500  flex items-center justify-between p-3 border rounded-lg "
+    }
+    taskelement.innerHTML = `
+    <div class="items-center">
+    <input 
+        type="checkbox" 
+        ${taskitem.status === "completed" ? "checked" : ""}
+        onchange="markComplete(${taskitem.id})"
+        class="w-6 h-6 border-2 rounded "
+      >
+    <h3 class ="font-semibold line-through flex justify-between"> ${
+      taskitem.name
+    }</h3>
+    <p class="text-gray-600 ">Due:${taskitem.dueDate}</p>
+    </div>
+    <div class="space-x-3">
+    <button  onclick="edit(${
+      taskitem.id
+    })" class="mt-2 border bg-orange-500 text-black px-3 py-1 rounded">Edit</button>
+    <button  onclick= "deletetask(${
+      taskitem.id
+    })" class="mt-2 bg-red-500 text-white px-3 py-1 rounded">Delete</button>
+    </div>
+    `
+    taskdiv.appendChild(taskelement)
+  })
 }
 showtask()
 save()
